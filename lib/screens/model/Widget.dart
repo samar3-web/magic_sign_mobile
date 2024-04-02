71818b146852e7723b51fbc9d0149c85028b9b58
samar ben hadj abbes <samarabbes3@gmail.com>
2024-04-02 13:07:37 +0100

    add remove and edit for widgets

--- lib/screens/model/Widget.dart
+++ lib/screens/model/Widget.dart
@@ -3,11 +3,12 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class WidgetData {
-   int? widgetId;
-   int? playlistId;
-   String? type;
-   int? duration;
-   String? displayOrder;
+  int? widgetId;
+  int? playlistId;
+  String? type;
+  int? duration;
+  String? displayOrder;
+  List<String>? mediaIds;
 
   WidgetData({
     required this.widgetId,
@@ -15,6 +16,7 @@ class WidgetData {
     required this.type,
     required this.duration,
     required this.displayOrder,
+    required this.mediaIds,
   });
   WidgetData.fromJson(Map<String, dynamic> json) {
     widgetId = json['widgetId'];
@@ -22,9 +24,6 @@ class WidgetData {
     type = json['type'];
     duration = json['duration'];
     displayOrder = json['displayOrder'];
- 
-  
-
+    mediaIds = json['mediaIds'] != null ? List<String>.from(json['mediaIds'] as List<dynamic>) : null; 
   }
-
 }
--- lib/screens/playlist/playlist_details.dart
+++ lib/screens/playlist/playlist_details.dart
@@ -97,7 +97,8 @@ class _PlaylistDetail extends State<PlaylistDetail> {
                               child: GridView.builder(
                                 padding: EdgeInsets.all(16.0),
                                 scrollDirection: Axis.horizontal,
-                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
                                   crossAxisCount: 2,
                                   crossAxisSpacing: 6.0,
                                   mainAxisSpacing: 10.0,
@@ -105,23 +106,30 @@ class _PlaylistDetail extends State<PlaylistDetail> {
                                 ),
                                 itemCount: mediaController.mediaList.length,
                                 itemBuilder: (context, index) {
-                                  Media media = mediaController.mediaList[index];
+                                  Media media =
+                                      mediaController.mediaList[index];
                                   return InkWell(
                                     onTap: () {
                                       //handle Media Pressed
                                       print("media pressed");
                                       print("playlistId: ");
-                                      print(widget.playlist.regions[0].playlists[0].playlistId);
+                                      print(widget.playlist.regions[0]
+                                          .playlists[0].playlistId);
                                       print("media: ");
                                       print(media.mediaId);
                                       playlistController.assignPlaylist(
-                                          [media.mediaId], widget.playlist.regions[0].playlists[0].playlistId);
+                                          [media.mediaId],
+                                          widget.playlist.regions[0]
+                                              .playlists[0].playlistId);
                                     },
                                     child: Container(
-                                      width: MediaQuery.of(context).size.width / 3 - 16,
+                                      width: MediaQuery.of(context).size.width /
+                                              3 -
+                                          16,
                                       decoration: BoxDecoration(
                                         color: Colors.white,
-                                        borderRadius: BorderRadius.circular(12.0),
+                                        borderRadius:
+                                            BorderRadius.circular(12.0),
                                         boxShadow: [
                                           BoxShadow(
                                             color: Colors.grey.withOpacity(0.3),
@@ -148,40 +156,127 @@ class _PlaylistDetail extends State<PlaylistDetail> {
                                 height: 250,
                                 width: MediaQuery.of(context).size.width,
                                 child: ListView.builder(
-                                  itemCount: playlistController.timelines!.length,
+                                  itemCount:
+                                      playlistController.timelines!.length,
                                   itemBuilder: (c, i) {
                                     return Padding(
                                       padding: const EdgeInsets.all(8.0),
                                       child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
                                         children: [
-                                          Wrap(
+                                          Row(
+                                            crossAxisAlignment: CrossAxisAlignment
+                                                .start, // Align children of Row to the start
                                             children: [
                                               Container(
-                                                child: Text('Timeline ${i}', style: TextStyle(color: Colors.white)),
-                                                alignment: Alignment.bottomLeft,
+                                                child: Text('Timeline ${i}',
+                                                    style: TextStyle(
+                                                        color: Colors.white)),
+                                                alignment: Alignment.center,
                                                 width: 120,
-                                                height: 50,
+                                                height: 74,
                                                 decoration: BoxDecoration(
                                                   color: kSecondaryColor,
                                                   border: Border.all(
                                                     color: kSecondaryColor,
                                                     width: 2,
                                                   ),
-                                                  borderRadius: BorderRadius.circular(10),
+                                                  borderRadius:
+                                                      BorderRadius.circular(10),
                                                 ),
                                               ),
-                                              SizedBox(width: 5),
-                                              // Parcours des playlists dans chaque timeline
-                                              for (Playlists playlist in playlistController.timelines![i].playlists!)
-                                                for (WidgetData widget in playlist.widgets!)
-                                                  Padding(
-                                                    padding: EdgeInsets.symmetric(horizontal: 2.0),
-                                                    child: Text(
-                                                      'Type: ${widget.type}',
-                                                      style: TextStyle(color: Colors.black),
-                                                    ),
+                                              Expanded(
+                                                child: SingleChildScrollView(
+                                                  scrollDirection:
+                                                      Axis.horizontal,
+                                                  child: Row(
+                                                    children: [
+                                                      // Parcours des playlists dans chaque timeline
+                                                      for (Playlists playlist
+                                                          in playlistController
+                                                              .timelines![i]
+                                                              .playlists!)
+                                                        for (WidgetData widget
+                                                            in playlist
+                                                                .widgets!)
+                                                          Padding(
+                                                            padding: EdgeInsets
+                                                                .symmetric(
+                                                                    horizontal:
+                                                                        1.0),
+                                                            child: Container(
+                                                              child: Column(
+                                                                crossAxisAlignment:
+                                                                    CrossAxisAlignment
+                                                                        .center,
+                                                                children: [
+                                                                  Text(
+                                                                    '${widget.type}',
+                                                                    style: TextStyle(
+                                                                        color: Colors
+                                                                            .white),
+                                                                    textAlign:
+                                                                        TextAlign
+                                                                            .center, // Align text to the center
+                                                                  ),
+                                                                  SizedBox(
+                                                                      height:
+                                                                          2), // Add space between text and buttons
+                                                                  Row(
+                                                                    mainAxisAlignment:
+                                                                        MainAxisAlignment
+                                                                            .center,
+                                                                    children: [
+                                                                      IconButton(
+                                                                        icon: Icon(
+                                                                            Icons.edit),
+                                                                        onPressed:
+                                                                            () {
+                                                                          // Add your edit onPressed logic here
+                                                                        },
+                                                                      ),
+                                                                      SizedBox(
+                                                                          width:
+                                                                              5), // Add space between buttons
+                                                                      IconButton(
+                                                                        icon: Icon(
+                                                                            Icons.delete),
+                                                                        onPressed:
+                                                                            () {
+                                                                          // Add your delete onPressed logic here
+                                                                        },
+                                                                      ),
+                                                                    ],
+                                                                  ),
+                                                                ],
+                                                              ),
+                                                              alignment:
+                                                                  Alignment
+                                                                      .center,
+                                                              width: 120,
+                                                              height: 74,
+                                                              decoration:
+                                                                  BoxDecoration(
+                                                                color: boxColor,
+                                                                border:
+                                                                    Border.all(
+                                                                  color:
+                                                                      boxColor,
+                                                                  width: 2,
+                                                                ),
+                                                                borderRadius:
+                                                                    BorderRadius
+                                                                        .circular(
+                                                                            10),
+                                                              ),
+                                                            ),
+                                                          ),
+                                    
+                                                ],
                                                   ),
+                                                ),
+                                              ),
                                             ],
                                           ),
                                         ],
@@ -195,14 +290,6 @@ class _PlaylistDetail extends State<PlaylistDetail> {
                         ),
                       ),
               ),
-              if (_showScrollIndicator)
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  child: Center(
-                    child: Icon(Icons.arrow_forward, color: Colors.grey),
-                  ),
-                ),
               Positioned(
                 bottom: 10,
                 left: 0,
